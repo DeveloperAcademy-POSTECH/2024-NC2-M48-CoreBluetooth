15330c6246407ad071184161ea21019419af2925
interstellaar <https://github.com/>
2024-06-17 12:13:18 +0900

    프로젝트 생성

---
+++ My Clicker/My Clicker/My_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_ClickerApp.swift
+//  My Clicker
+//
+//  Created by Jeho Ahn on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct My_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
