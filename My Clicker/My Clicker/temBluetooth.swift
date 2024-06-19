e92a07d2874ac4db60643ee6342b724d9df9f08c
seeeRini <serin2669@naver.com>
2024-06-20 03:32:55 +0900

    [View연결] 임시뷰(temBluetooth와 addSheet) 포함 모든 뷰 NevigationLink 완료

---
+++ My Clicker/My Clicker/temBluetooth.swift
@@ -0,0 +1,18 @@
+//
+//  temBluetooth.swift
+//  My Clicker
+//
+//  Created by 세린맥북 on 6/20/24.
+//
+
+import SwiftUI
+
+struct temBluetooth: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    temBluetooth()
+}
